382b183a7e6015787b78dffa7e1adae289186f2d
Ming-doan <quangminh57dng@gmail.com>
2024-03-11 15:17:54 +0700

    sprint1/feat: Update CV control

--- apps/controllers/cv_controller.py
+++ apps/controllers/cv_controller.py
@@ -1,7 +1,9 @@
 from typing import Any, Dict
 from .extract_controller import extract_control
+from ..models.cv_model import CVModel
 from ..providers.db_provider import DatabaseProvider
 from ..providers.vectordb_provider import VectorDatabaseProvider
+from ..providers.storage_provider import StorageProvider
 from ..utils.system_prompt import system_prompt_cv
 from ..utils.mock import default_fmt
 
@@ -9,6 +11,7 @@ from ..utils.mock import default_fmt
 # Define the database and vector database provider
 database = DatabaseProvider(collection_name="CVs")
 vector_database = VectorDatabaseProvider(size=96)
+storage = StorageProvider(directory="CVs")
 
 
 def cv_control(file: bytes, filename: str, user_id: str) -> Dict[str, Any]:
@@ -18,6 +21,7 @@ def cv_control(file: bytes, filename: str, user_id: str) -> Dict[str, Any]:
     # Load bytes as a files with filename
 
     # Upload file to Firebase storage
+    path, url = storage.upload(file, filename)
 
     # Convert the file to raw text
     raw_text = filename
@@ -26,37 +30,41 @@ def cv_control(file: bytes, filename: str, user_id: str) -> Dict[str, Any]:
     criteria = default_fmt
 
     # Extract features from the raw text
-    extraction, word_embeddings = extract_control(
-        system_prompt=system_prompt_cv, prompt=raw_text, fmt=criteria
-    )
+    # extraction, word_embeddings = extract_control(
+    #     system_prompt=system_prompt_cv, prompt=raw_text, fmt=criteria
+    # )
 
     # Format data to upload
-    cv_data = {
-        "extraction": extraction,
-    }
+    cv_data = CVModel(
+        name=filename,
+        path=path,
+        url=url,
+        extraction={}
+    ).to_dict()
 
     # Upload the extraction to the database
-    # data_id = database.create(data=cv_data)
+    data_id = database.create(data=cv_data)
+    cv_data["id"] = data_id
 
     # Upload vector to the database
-    payload = {
-        "id": "test_id",
-    }
-    for key, value in word_embeddings.items():
-        # Get collection name
-        collection_name = f"cv_{key}_{user_id}"
-        # If value is a list
-        if isinstance(value, list):
-            for item in value:
-                vector_database.insert(
-                    collection_name=collection_name, array=item, data=payload)
+    # payload = {
+    #     "id": "test_id",
+    # }
+    # for key, value in word_embeddings.items():
+    #     # Get collection name
+    #     collection_name = f"cv_{key}_{user_id}"
+    #     # If value is a list
+    #     if isinstance(value, list):
+    #         for item in value:
+    #             vector_database.insert(
+    #                 collection_name=collection_name, array=item, data=payload)
 
-        elif isinstance(value, dict):
-            for k, v in value.items():
-                vector_database.insert(
-                    collection_name=collection_name, array=v, data=payload)
-        else:
-            vector_database.insert(
-                collection_name=collection_name, array=value, data=payload)
+    #     elif isinstance(value, dict):
+    #         for k, v in value.items():
+    #             vector_database.insert(
+    #                 collection_name=collection_name, array=v, data=payload)
+    #     else:
+    #         vector_database.insert(
+    #             collection_name=collection_name, array=value, data=payload)
 
     return cv_data
