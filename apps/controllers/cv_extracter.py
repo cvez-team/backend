21ae20531a2f5ff14c393690d1f1ca76b61066d4
Hoangnt1209 <nth120923@gmail.com>
2024-03-11 14:28:04 +0700

    Srpint1/chore : Add Raise Error for PDF or Docx File

--- apps/controllers/cv_extracter.py
+++ apps/controllers/cv_extracter.py
@@ -1,25 +1,30 @@
 from langchain.document_loaders import Docx2txtLoader, PyPDFLoader
 import os
-
+import io
 
 def load_pdf_docx(file_path):
     if os.path.basename(file_path).endswith(".pdf") or os.path.basename(file_path).endswith(".PDF"):
         loader =  PyPDFLoader(file_path)
     elif os.path.basename(file_path).endswith(".docx") or os.path.basename(file_path).endswith(".DOCX"):
         loader = Docx2txtLoader(file_path)
+    else :
+        raise ValueError("Not the PDF or DOCX file ")
     
     documents = loader.load_and_split()
 
     return documents
 
-def get_cv(file_name):
-    file_path = file_name
-
+def get_cv(filename: str):
+    file_path = filename
     data = load_pdf_docx(file_path=file_path)
+
     _context = ""
     for x in data:
-        lines = x.page_content.split('\n')
-        non_empty_lines = [line.strip() for line in lines if line.strip()]
-        _context += '\n'.join(non_empty_lines) + "\n"
+        non_empty_lines = [line.strip() for line in x.page_content.splitlines() if line.strip()]
+        _context += "\n".join(non_empty_lines) + "\n"
 
-    return _context.strip()
+    return _context
+
+file = "Input file Docx/PDF"
+cv_text = get_cv(file)
+print(cv_text)
