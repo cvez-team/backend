9e8fa1ede50218f7698f0339b11758d84130dd4d
DiepDong <diepgiadong@gmail.com>
2024-03-12 15:02:00 +0700

    sprint1/feat: add query_controller.py

---
+++ apps/controllers/query_controller.py
@@ -0,0 +1,28 @@
+from ..providers.vectordb_provider import VectorDatabaseProvider
+from ..utils.mock import default_fmt
+from typing import Any, Tuple, List
+
+vector_database = VectorDatabaseProvider(size=768)
+tag ={
+    "cv": "cv",
+    "jd": "jd",
+    "questions": "questions"
+}
+
+
+def query_controller(tag: str, user_id: str, id_filebase: str, fmt=default_fmt) -> List[Tuple[str, Any, Any]]:
+    '''
+    Query the vector database based on a specific tag, user_id, id_filebase.
+    Return a list of tuples containing the id, payload, and vector.
+    '''
+    key = fmt.keys()
+    if tag not in tag:
+        raise ValueError(f"Tag '{tag}' is not defined.")
+
+    collection = f"{tag}_{key}_{user_id}"
+    for collection_name in collection:
+        query_results = vector_database.query(collection_name=collection_name, key='id', value=id_filebase)
+    
+    return query_results
+
+
