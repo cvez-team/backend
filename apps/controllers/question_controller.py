9f6658f21a718186033175f80d12b3564ef9524b
thultm <90077888+thultm@users.noreply.github.com>
2024-03-12 22:48:17 +0700

    sprint1/feat: add question_controller (#6)
    * sprint1/feat: add question_controller
    
    * sprint1/feat: Update question_controller and system_prompt_question in system_prompt
    
    * sprint1/feat: Update question_controller and system_prompt_question in system_prompt
    
    * sprint1/update: Create add database function

---
+++ apps/controllers/question_controller.py
@@ -0,0 +1,68 @@
+from typing import Any, Dict, List
+from .extract_controller import extract_control
+from ..providers.db_provider import DatabaseProvider
+from ..providers.vectordb_provider import VectorDatabaseProvider
+from ..utils.system_prompt import system_prompt_question
+from ..utils.mock import default_fmt, question_fmt
+from ..models.question_model import QuestionModel
+import re
+
+# Define the database and vector database provider
+database = DatabaseProvider(collection_name="Questions")
+vector_database = VectorDatabaseProvider(size=768)
+
+def question_control(title: str, content: str, answer: str) -> Dict[str, Any]:
+    '''
+    question_control is a function that controls the creation of a new question.
+    
+    Args:
+    - content (List[str]): The content of the questions.
+    
+    Returns:
+    - question_data (dict): A dictionary containing the keywords extracted from the content.
+    '''
+    # Retrieve the raw text
+    raw_text = "\n".join(content)
+    # Remove invalid characters from the raw text (only allow alphanumeric characters and spaces)
+    raw_text = re.sub(r"[^a-zA-Z0-9\s]", "", raw_text)
+    
+    # Fetch extract criterias
+    criteria = default_fmt
+
+    # Extract features from the raw text
+    extraction, word_embeddings = extract_control(
+        system_prompt=system_prompt_question, prompt=raw_text, fmt=criteria
+    )
+    # Create a dictionary for the question data
+    question_data = QuestionModel(
+        title=title,
+        content=content,
+        answer=answer,
+        extraction=extraction
+    ).to_dict()
+    
+    # Upload the extraction to the database
+    question_id = database.create(data=question_data)
+    
+    # Create a payload for the vector database
+    payload = {
+        "id": question_id
+    }
+    
+    for key, value in word_embeddings.items():
+        # Get collection name
+        collection_name = f"question_{key}_cvez"    
+        # If value is a list
+        if isinstance(value, list):
+            for item in value:
+                vector_database.insert(
+                    collection_name=collection_name, array=item, data=payload)
+
+        elif isinstance(value, dict):
+            for k, v in value.items():
+                vector_database.insert(
+                    collection_name=collection_name, array=v, data=payload)
+        else:
+            vector_database.insert(
+                collection_name=collection_name, array=value, data=payload)
+    return question_data
--- apps/utils/mock.py
+++ apps/utils/mock.py
@@ -1,26 +1,39 @@
 default_fmt = {
-    # "Degree": {
-    #     "type": "string",
-    #     "description": "Degree of the candidate. e.g. B.Tech, M.Tech, B.Sc, M.Sc"
-    # },
-    # "Experience": {
-    #     "type": "list",
-    #     "description": "Experience of the candidate in years."
-    # },
+    "Degree": {
+        "type": "list",
+        "description": "Degree of the candidate. e.g. B.Tech, M.Tech, B.Sc, M.Sc"
+    },
+    "Experience": {
+        "type": "list",
+        "description": "Experience of the candidate in years."
+    },
     "TechnicalSkills": {
         "type": "list",
         "description": "List of technical skills of the candidate."
+    },
+    "Responsibilities": {
+        "type": "list",
+        "description": "List of responsibilities of the candidate."
+    },
+    "Certifications": {
+        "type": "list",
+        "description": "List of certifications of the candidate."
+    },
+    "SoftSkills": {
+        "type": "list",
+        "description": "List of soft skills of the candidate."
     }
-    # "Responsibilities": {
-    #     "type": "list",
-    #     "description": "List of responsibilities of the candidate."
-    # },
-    # "Certifications": {
-    #     "type": "list",
-    #     "description": "List of certifications of the candidate."
-    # },
-    # "SoftSkills": {
-    #     "type": "list",
-    #     "description": "List of soft skills of the candidate."
-    # },
 }
+
+question_fmt = {
+        "type": "list",
+        "description": "Category of the keywords. e.g. Problem-solving, Leadership, Networking, etc.",
+        "items": {
+            "type": "string",
+            "description": "Keywords extracted from the question."
+            }
+        }
+
+    
+    
+            
