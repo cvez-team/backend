a519fd1db49dd8194710823e3c324c0762060de9
Hoangnt1209 <nth120923@gmail.com>
2024-03-13 13:44:36 +0700

    Sprint1/feat:Add delete_controller and route delete

--- apps/controllers/delete_controller.py
+++ apps/controllers/delete_controller.py
@@ -1,10 +1,10 @@
 from ..providers.db_provider import DatabaseProvider
+from ..utils.constants import CV_COLLECTION, JD_COLLECTION, QUESTION_COLLECTION
 
 
-cv_database = DatabaseProvider(collection_name="CVs")
-jd_database = DatabaseProvider(collection_name="JDs")
-question_database = DatabaseProvider(collection_name="Questions")
-
+cv_database = DatabaseProvider(collection_name=CV_COLLECTION)
+jd_database = DatabaseProvider(collection_name=JD_COLLECTION)
+question_database = DatabaseProvider(collection_name=QUESTION_COLLECTION)
 
 def delete_jd_by_id_control(jd_id: str):
     return jd_database.delete(jd_id)
