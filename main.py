add2fe79aa2fe179cccccf986c17322bdaecb23f
DiepDong <diepgiadong@gmail.com>
2024-03-13 10:12:30 +0700

    sprint1/feat: add router update

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from apps.utils.create_app import create_app
 from apps.routes.new import router as uploads_router
 from apps.routes.match import router as match_router
 from apps.routes.get import router as get_router
+from apps.routes.update import router as update_router
 from dotenv import load_dotenv
 
 
@@ -17,7 +18,7 @@ app = create_app()
 app.include_router(uploads_router, prefix="/api")
 app.include_router(match_router, prefix="/api")
 app.include_router(get_router, prefix="/api")
-
+app.include_router(update_router, prefix="/api")
 
 # Launch FastAPI app
 if __name__ == "__main__":
