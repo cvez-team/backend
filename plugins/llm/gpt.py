7dad8e54fea12a39c6fe30671231ef2c5f06490f
dtduy77 <duythduong.2003@gmail.com>
2024-03-13 14:28:34 +0700

    sprint1/duy: chore add more promt

--- plugins/llm/gpt.py
+++ plugins/llm/gpt.py
@@ -2,5 +2,5 @@ import os
 from langchain_openai import ChatOpenAI
 
 
-model = ChatOpenAI(api_key=os.environ.get('OPENAI_API_KEY'), temperature=0)
+model = ChatOpenAI(api_key=os.environ.get('OPENAI_API_KEY'), temperature=0, request_timeout=120)
 
